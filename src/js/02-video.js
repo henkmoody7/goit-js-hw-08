9fcd69cd80370991d4646cac1274755ec0e60247
Mykola Harasym <henkmoody7@gmail.com>
2022-08-16 23:23:39 +0300

    finish task1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,29 @@
-// const iframe = document.querySelector('#vimeo-player');
-console.log('jkabsda');
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+// function countTime(e) {
+//   const videoTime = e.seconds;
+//   console.log(videoTime);
+//   return videoTime;
+// }
+
+// player.on('timeupdate', countTime);
+
+// player
+//   .setCurrentTime()
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
