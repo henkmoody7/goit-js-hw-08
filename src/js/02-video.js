e1f1e75bfbdcd4555dbf21e797ff272ab2eac5ca
Mykola Harasym <henkmoody7@gmail.com>
2022-08-17 22:10:26 +0300

    did task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,28 +2,29 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const storageTime = localStorage.getItem('videoplayer-current-time');
 
-// function countTime(e) {
-//   const videoTime = e.seconds;
-//   console.log(videoTime);
-//   return videoTime;
-// }
+player.on('timeupdate', countTime);
 
-// player.on('timeupdate', countTime);
+function countTime(e) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
+}
 
-// player
-//   .setCurrentTime()
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
+if (localStorage.getItem('videoplayer-current-time')) {
+  player
+    .setCurrentTime(storageTime)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
 
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
